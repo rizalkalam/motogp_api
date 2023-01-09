31d4002355edf5787219cc1c76933075ba26b523
rizalkalam <rizalkalam78@gmail.com>
2023-01-09 18:46:05 +0700

    first commit

---
+++ database/migrations/2023_01_06_140918_create_riders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('riders', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('number');
+            $table->string('nationality');
+            $table->string('team_name');
+            $table->string('team_bike');
+            $table->integer('rider_standings');
+            $table->double('rider_points');
+            $table->foreignId('team_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('riders');
+    }
+};
---
+++ database/migrations/2023_01_06_141340_create_teams_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('bike');
+            $table->string('team_status');
+            $table->integer('team_standings');
+            $table->double('team_points');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+};
